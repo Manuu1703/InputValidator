d43dbabe55dcd2286017881961c0e7bad223bbbc
Manuu1703 <manuapu1739@gmail.com>
2023-07-10 21:55:06 +0530

    Initial commit for repository creation

---
+++ src/test/java/com/learn/InputValidator/InputValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.InputValidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InputValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
